d2fa2958d6b240b67ebe3a108ddee2dcb30541f6
Hoyrat <53577734+Hoyrat@users.noreply.github.com>
2019-12-16 19:50:53 +0100

    Update login.java
    update from github

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -3,5 +3,7 @@ package tests;
 public class login {
     public static void main(String[] args) {
         System.out.println("this is a new try");
+        
+        //bu satirlar githubtan
     }
 }
